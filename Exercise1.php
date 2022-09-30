944e975f47473f5bf7b8d9eef60e5f9f3869ea36
ronelanastacio <89885308+ronelanastacio@users.noreply.github.com>
2022-09-30 10:57:56 +0800

    Add files via upload

---
+++ Exercise1.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+	<title>
+		Exercise 1
+	</title>
+</head>
+<body bgcolor = "blue">
+<center>
+<h1>
+	This is my Exercise 1 in PHP!
+</h1>
+<?php
+	//declare variables
+	$full_name = "Ronel B. Anastacio Jr.";
+	$college_program = "B.S. in Computer Engineering";
+	$year = "3rd Year";
+	$section = "WDD - 58043";
+	$string = "Today, I will learn PHP programming.";
+	
+	//print variables
+	echo "My name is $full_name.<br><br> I am a $year student currently taking $college_program and I am part of $section.<br><br>";
+	echo $string;
+	
+?>
+
+
+</body>
+</html>
